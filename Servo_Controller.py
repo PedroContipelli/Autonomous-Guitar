bd7d80edb0f270b786a7ca2ba14ef4d15784e205
PedroContipelli <Pcontipelli10@hotmail.com>
2023-02-06 14:20:33 -0500

    Connect play to servo_controller

--- Servo_Controller.py
+++ Servo_Controller.py
@@ -2,12 +2,14 @@ from machine import Pin
 import time
 
 led = Pin(2, Pin.OUT)
-SERIAL_OUT = Pin(0, Pin.OUT)
+SER = Pin(0, Pin.OUT)
 RCLK = Pin(4, Pin.OUT)
 SRCLK = Pin(16, Pin.OUT)
 
+cycles = 10
+
 def output(value):
-  SERIAL_OUT.value(value)
+  SER.value(value)
 
 def shift(n):
   for i in range(n):
@@ -18,27 +20,27 @@ def store():
   RCLK.value(1)
   RCLK.value(0)
 
-cycles = 10
+def servo_write(servo_states):
+  # incoming servo_states is 1-indexed for physical convention reasons
+  num_servos = len(servo_states) - 1
 
-def servo_write(servo_state):
   for _ in range(cycles):
     min_angle = 100
     max_angle = 900
 
     output(1)
-    shift(30)
+    shift(num_servos)
     store()
     time.sleep_us(min_angle)
 
-    num_servos = 30
-
-    for i in range(num_servos):
-      output(servo_state[num_servos - i - 1])
+    for servo in range(num_servos, 0, -1):
+      output(servo_states[servo])
       shift(1)
+
     store()
-    time.sleep_us(max_angle-min_angle)
+    time.sleep_us(max_angle - min_angle)
 
     output(0)
     shift(num_servos)
     store()
-    time.sleep_us(20000-max_angle)
+    time.sleep_us(20_000 - max_angle)
--- play.py
+++ play.py
@@ -4,7 +4,7 @@ from Servo_Controller import servo_write
 
 play_file = "Twinkle_Output"
 
-servo_state = [False for i in range(31)]
+servo_states = [0 for i in range(31)]
 last_fret_played_on = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
 
 def play_guitar_note(msg, print_human_notes=False, debug=False):
@@ -15,27 +15,26 @@ def play_guitar_note(msg, print_human_notes=False, debug=False):
     if previous_fret != fret_motor:
         # UNFRET OLD
         if previous_fret != 0:
-            servo_state[previous_fret] = False
+            servo_states[previous_fret] = 0
             if debug:
                 print(f"OFF {servo_label[previous_fret]} (Motor # {previous_fret})")
 
         # FRET NEW
         if fret_motor != 0:
-            servo_state[fret_motor] = True
+            servo_states[fret_motor] = 1
             if debug:
                 print(f"ON {servo_label[fret_motor]} (Motor # {fret_motor})")
 
     # STRUM
-    servo_state[strum_motor] = not servo_state[strum_motor]
+    servo_states[strum_motor] = 1 - servo_states[strum_motor]
     if debug:
         print(f"{servo_label[strum_motor]} (Motor # {strum_motor})\n")
 
     # Update last note played
     last_fret_played_on[strum_motor] = fret_motor
 
-
     # WRITE ALL SERVOS
-    servo_write(servo_state)
+    servo_write(servo_states)
 
     if print_human_notes:
         print("Playing", human_notes[msg.note])
