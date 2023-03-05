5cd87b86f0183aed1b0f3490570f64ee25cecb6d
PedroContipelli <Pcontipelli10@hotmail.com>
2023-03-04 21:11:05 -0500

    Refactor servo_controller

--- ESP32/Servo_Controller.py
+++ ESP32/Servo_Controller.py
@@ -10,48 +10,47 @@ led = Pin(2, Pin.OUT)
 SER = Pin(0, Pin.OUT)
 RCLK = Pin(4, Pin.OUT)
 SRCLK = Pin(16, Pin.OUT)
-
 num_servos = 30
-cycles = 30
-extra_wait_time = 0.5 # in seconds
 
 def main_test():
-    # Resets to starting position
+    # mapping()
     alignment()
 
-    # Test all in this range (inclusive)
-    while True:
-        test_range(min=1, max=6, reps=1)
-
-    # FOR MAPPING:
-    # Run test_range() with reps=3 and write down sequence of which physical
-    # servos are played in left column of "physical_map" in LookupTables.py
-
-# Set all to 0 or 1
-def alignment(set_to=0):
-    virtual_states = [set_to for _ in range(num_servos + 1)]
-    previous_states = virtual_states.copy()
-    previous_states = servo_write(virtual_states, previous_states)
-    time.sleep(extra_wait_time)
-
-# Counts up powering each servo ON and OFF once
-def test_range(min=1, max=num_servos, reps=1):
+    test_range_loop(min=25, max=30, reps=1, wait_time=0.30, cycles=20)
+        
+def alignment(at_a_time=3):
     virtual_states = [0 for _ in range(num_servos + 1)]
     previous_states = virtual_states.copy()
     
-    for servo in range(min, max + 1):
-      for _ in range(reps):
-          print(f"Servo #{servo} ON\n")
-          virtual_states[servo] = 1
-          previous_states = servo_write(virtual_states, previous_states)
+    for servo in range(1, num_servos, at_a_time):
+        print(f"Aligning Servos #{servo} - {servo + at_a_time - 1}") 
+        for s in range(servo, servo + at_a_time):
+            previous_states[s] = 1
+        previous_states = servo_write(virtual_states, previous_states, cycles=10)
+        time.sleep(0.15)
+    
+def mapping():
+    test_range_loop(reps=3, wait_time=1, cycles=10)
 
-          print(f"Servo #{servo} OFF\n")
-          virtual_states[servo] = 0
-          previous_states = servo_write(virtual_states, previous_states)
-          time.sleep(extra_wait_time)
+# Counts up powering each servo ON and OFF once
+def test_range_loop(min=1, max=30, reps=1, wait_time=0.5, cycles=15):
+    while True:
+        virtual_states = [0 for _ in range(num_servos + 1)]
+        previous_states = virtual_states.copy()
+        
+        for servo in range(min, max + 1):
+          for _ in range(reps):
+              print(f"Servo #{servo} ON\n")
+              virtual_states[servo] = 1
+              previous_states = servo_write(virtual_states, previous_states, cycles=cycles)
+
+              print(f"Servo #{servo} OFF\n")
+              virtual_states[servo] = 0
+              previous_states = servo_write(virtual_states, previous_states, cycles=cycles)
+              time.sleep(wait_time)
 
 def output(value):
-  SER.value(value)
+    SER.value(value)
 
 def shift(n):
   for i in range(n):
@@ -62,27 +61,33 @@ def store():
   RCLK.value(1)
   RCLK.value(0)
 
-def servo_write(servo_states, previous_states):
+def servo_write(servo_states, previous_states, cycles=15):
   # incoming servo_states is 1-indexed for physical convention reasons
   num_servos = len(servo_states) - 1
+  fret_min_angle = 300 # microseconds (smaller = fret lower)
+  fret_max_angle = 500 # microseconds (larger = unfret higher)
+  strum_min_angle = 300 # currently not doing anything...
+  strum_max_angle = 500 # currently not doing anything...
 
   for _ in range(cycles):
-    min_angle = 200 # microseconds
-    max_angle = 400 # microseconds
-        
     # Controls whether servo is operating or completely OFF
     for virtual_servo in range(num_servos, 0, -1):
       # Allows adaptability to physical wiring changes
       true_servo = physical_map[virtual_servo]
       
       prev_state = previous_states[true_servo]
-      current_state = servo_states[true_servo]
+      new_state = servo_states[true_servo]
       
-      # We only want servo to be COMPLETELY OFF after it has already unfretted
-      output(prev_state or current_state)
+      # Strumming servos only powered ON whenever there's a change in state
+      if 25 <= virtual_servo <= 30:
+          output(prev_state ^ new_state)
+      # Fretting servos only powered OFF when remaining in the up position
+      else:
+          output(prev_state or new_state)
+
       shift(1) # Only shifting a single bit into one servo
     store()
-    time.sleep_us(min_angle)
+    time.sleep_us(fret_min_angle)
 
     # Serial data loaded in from front to back (reverse order)
     for virtual_servo in range(num_servos, 0, -1):
@@ -90,19 +95,19 @@ def servo_write(servo_states, previous_states):
       true_servo = physical_map[virtual_servo]
       
       prev_state = previous_states[true_servo]
-      current_state = servo_states[true_servo]
+      new_state = servo_states[true_servo]
       
-      # We only want servo to FRET UP if previous state was fret down
-      output((not current_state) and prev_state)
-      
+      # The reason this works is complicated... but the algebra simplifies to this (strumming & fretting!)
+      output((not new_state) and prev_state)
+
       shift(1) # Only shifting a single bit into one servo
     store()
-    time.sleep_us(max_angle - min_angle)
+    time.sleep_us(fret_max_angle - fret_min_angle)
 
     output(0)
     shift(num_servos) # Shifting 0 into ALL servos
     store()
-    time.sleep_us(20_000 - max_angle)
+    time.sleep_us(20_000 - fret_max_angle)
 
   # Return new "previous_states"
   return servo_states.copy()
