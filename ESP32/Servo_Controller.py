99253e944f92849c86d71751680f695579448ec5
PedroContipelli <Pcontipelli10@hotmail.com>
2023-03-08 03:23:33 -0500

    Working Angle Control

--- ESP32/Servo_Controller.py
+++ ESP32/Servo_Controller.py
@@ -14,13 +14,17 @@ SRCLK = Pin(16, Pin.OUT)
 num_servos = 30
 num_fret_servos = 24
 
-fret_min_angle, fret_max_angle = (120, 140) # (fret lower <--> unfret higher)
-strum_min_angle, strum_max_angle = (0, 180)
-cycle_duration = 10_000 # microseconds
+# fret_min_angle, fret_max_angle = (0, 0) # (fret lower <--> unfret higher)
+# strum_min_angle, strum_max_angle = (130, 150)
 
-# Values hardcoded for our specific servo model
-def angle_to_microseconds(angle):
-    return (angle/180) * 2000 + 500
+cycle_duration = 9_500 # microseconds
+
+# # Values hardcoded for our specific servo model
+# def angle_to_us(angle):
+#     return int((angle/180) * 2000 + 500)
+
+fret_min_us, fret_max_us = (200, 400)
+strum_min_us, strum_max_us = (400, 500)
 
 def main_test():
     # mapping()
@@ -64,40 +68,41 @@ def test_range_loop(min=1, max=30, reps=1, wait_time=0.5, cycles=15):
 
 # Note: Servo states are 1-indexed (by physical convention)
 def servo_write(cycles=15):
-  global virtual_states, previous_states
+  global virtual_states, previous_states, fret_outputs, strum_outputs
   fret_outputs = precompute_stage_outputs(fret=True)
   strum_outputs = precompute_stage_outputs(strum=True)
 
   for cycle in range(cycles):
     a = time.time_ns()
-    stage1(fret_outputs, fret=True)
+    stage1(fret=True)
     b = time.time_ns()
-    stage2(fret_outputs, fret=True)
+    stage2(fret=True)
     c = time.time_ns()
-    stage3(fret_outputs, fret=True)
+    stage3(fret=True)
     d = time.time_ns()
 
-    stage1(strum_outputs, strum=True)
+    stage1(strum=True)
     e = time.time_ns()
-    stage2(strum_outputs, strum=True)
+    stage2(strum=True)
     f = time.time_ns()
-    stage3(strum_outputs, strum=True)
+    stage3(strum=True)
     g = time.time_ns()
-    all = [x//1000 for x in (b-a, c-b, d-c, e-d, f-e, g-f)]
-    print(all)
+
+  all = [x//1000 for x in (b-a, c-b, d-c, e-d, f-e, g-f)]
+  print(all)
 
   # Becomes new "previous_states"
   return virtual_states.copy()
 
 def precompute_stage_outputs(fret=False, strum=False):
-    outputs = [(0,0) for _ in range(num_servos + 1)]
+    outputs = [[0,0] for _ in range(num_servos + 1)]
 
     for virtual_servo in range(num_servos, 0, -1):
         prev_state, new_state = get_state(virtual_servo) # Accounts for physical mapping
 
         if virtual_servo <= num_fret_servos:
             outputs[virtual_servo][0] = fret and (prev_state or new_state) # FRET STAGE 1
-            outputs[virtual_servo][1] = fret and (not new_state) and prev_state) # FRET STAGE 2
+            outputs[virtual_servo][1] = fret and (not new_state) and prev_state # FRET STAGE 2
         else:
             outputs[virtual_servo][0] = strum and (prev_state ^ new_state) # STRUM STAGE 1
             outputs[virtual_servo][1] = strum and (not new_state) and prev_state # STRUM STAGE 2
@@ -105,35 +110,34 @@ def precompute_stage_outputs(fret=False, strum=False):
     return outputs
 
 # 0 if servo is OFF | 1 if servo set to at least min_angle
-def stage1(outputs, fret=False, strum=False):
+def stage1(fret=False, strum=False):
+    outputs = fret_outputs if fret else strum_outputs
     for virtual_servo in range(num_servos, 0, -1):
         output(outputs[virtual_servo][0])
         shift() # Shift a single bit into one servo
     store() # Buffer release after loop has finished
 
-    sleep_time_us = angle_to_microseconds(fret_min_angle if fret else strum_min_angle)
-    time.sleep_us(sleep_time_us)
+    time.sleep_us(fret_min_us if fret else strum_min_us)
 
 # 0 if servo is OFF | 1 if servo is pushed further to max angle
-def stage2(outputs, fret=False, strum=False):
+def stage2(fret=False, strum=False):
+    outputs = fret_outputs if fret else strum_outputs
     for virtual_servo in range(num_servos, 0, -1):
         output(outputs[virtual_servo][1])
         shift() # Shift a single bit into one servo
     store() # Buffer release after loop has finished
 
-    sleep_time_us = 0
-    if strum:
-        sleep_time_us = angle_to_microseconds(strum_max_angle - strum_min_angle)
-    elif fret:
-        sleep_time_us = angle_to_microseconds(fret_max_angle - fret_min_angle)
-    time.sleep_us(sleep_time_us)
+    if fret:
+        time.sleep_us(fret_max_us - fret_min_us)
+    else:
+        time.sleep_us(strum_max_us - strum_min_us)
 
 # Always 0 (end square wave, complete duty cycle)
 def stage3(fret=False, strum=False):
     output(0)
     shift(num_servos) # Shifts 0 into ALL servos
     store() # Buffer release
-    time.sleep_us(cycle_duration - angle_to_microseconds(fret_max_angle if fret else strum_max_angle))
+    time.sleep_us(cycle_duration - (fret_max_us if fret else strum_max_us))
 
 def get_state(virtual_servo):
     true_servo = physical_map[virtual_servo] # Adapts to physical wiring changes
